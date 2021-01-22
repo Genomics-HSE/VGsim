2c73f5821ace03ad2a38cad3f7fe77a495bbf471
Vladimir Shchur <vlshchur@gmail.com>
2021-01-23 01:20:43 +0300

    Complex rate calculation added

--- BirthDeath.py
+++ BirthDeath.py
@@ -1,6 +1,9 @@
-import numpy
+import numpy as np
 import random
 import sys
+
+def print_err(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
 
 class NodeS: #C-like structure
     def __init__(self):
@@ -15,16 +18,14 @@ class NeutralMutations:
         return self.muRate
 
 class BirthDeathModel:
-    def __init__(self, _B_rate, _D_rate, _S_rate, _M_rate = 0, **kwargs):
-        self.B_rate = _B_rate
-        self.D_rate = _D_rate
-        self.S_rate = _S_rate
+    def __init__(self, bRate, dRate, sRate, mRate = 0, **kwargs):
+
         self.Tree = [-1, 0, 0]
         self.genealogy = []
         self.nodeSampling = [NodeS() for _ in range(3)]
         self.times = [0]*3
         self.genealogyTimes = []
-        self.liveBranches = [1,2]
+        #self.liveBranches = [1,2]
         self.totalTime = 0.0
         self.UpdateRate()
         self.debug = False
@@ -32,6 +33,44 @@ class BirthDeathModel:
             if kwargs["debug"]:
                 self.debug = True
         #self.TypeOfMutations = [[0] * len(_M_rate[0])]
+
+    def SetRates(self, bRate, dRate, sRate, mRate):
+        if len(mRate) > 0:
+            self.dim = len(mRate[0])
+        else:
+            self.dim = 0
+        self.hapNum = int(4**self.dim)
+        if len(bRate) != self.hapNum or len(dRate) != self.hapNum or len(sRate) != self.hapNum:
+            print_err("BirthDeathModel.SetRates() fatal error: inconsistent dimension")
+            sys.exit(1)
+        for el in mRate:
+            if len(bRate) != self.hapNum:
+                print_err("BirthDeathModel.SetRates() fatal error: inconsistent dimension")
+                sys.exit(1)
+        self.bRate = bRate
+        self.dRate = dRate
+        self.sRate = sRate
+        self.mRate = mRate
+        self.liveBranches = [[] for _ in range(self.hapNum)]
+        self.liveBranches[0] += [1,2]
+        self.tRate = [0]*self.hapNum
+
+    def CalculateRate(self):
+        self.totalRate = 0
+        for i in range(hapNum):
+            tRate = self.bRate[i] + self.dRate[i] + self.sRate[i]
+            if self.dim > 0:
+                for el in self.mRate[i]:
+                    tRate += el
+            self.tRate[i] = tRate * len( self.liveBranches[i] )
+            self.totalRate += self.tRate[i]
+
+    def GenerateEvent(self):
+        haplotype = np.random.choice( range(self.hapNum), p = [el/self.totalRate for el in self.tRate] )
+        tRate =  self.tRate[haplotype] / len( self.liveBranches[i] )
+        prbs = [ self.bRate[haplotype]/tRate, self.dRate[haplotype]/tRate, self.sRate[haplotype]/tRate ] + [ el/tRate for el in self.mRate[haplotype] ]
+        eventType = np.random.choice( range(self.hapNum), p = prbs )
+        affectedBranch = np.random.randint(len(self.liveBranches[haplotype]))
 
     def GetGenealogy(self):
         for i in range(len(self.Tree) - 1, 0, -1):
@@ -58,7 +97,7 @@ class BirthDeathModel:
 
     def SampleTime(self, affectedBranch):
         #Проверить параметр в expo
-        tau = numpy.random.exponential(self.totalRate) ## А точно ли тут R_rate, если что упростить
+        tau = np.random.exponential(self.totalRate)
         self.totalTime += tau
         self.times[affectedBranch] = self.totalTime
 
@@ -87,7 +126,7 @@ class BirthDeathModel:
         for j in range(0, iterations):
             self.UpdateRate()
             eventType = random.random()
-            affectedBranch = numpy.random.randint(len(self.liveBranches)) #random.choice(liveBranches)
+            affectedBranch = np.random.randint(len(self.liveBranches)) #random.choice(liveBranches)
             self.SampleTime(affectedBranch)
             if eventType < self.B_rate[0]/self.totalRate:
                 self.Birth(affectedBranch)
