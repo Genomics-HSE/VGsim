5c143401f9c4c8a0bb2cb8b2d4edf7bd10169218
Spirin Vadim <61345894+spirinvadim@users.noreply.github.com>
2021-11-04 21:08:21 +0300

    Update vgsim.py

--- vgsim.py
+++ vgsim.py
@@ -117,7 +117,7 @@
 
 import VGsim
 simulator = VGsim.Simulator(2, 3, 3, 1234)
-simulator.set_infectious_rate(40)
+simulator.set_transmission_rate(40)
 simulator.set_recovery_rate(15)
 simulator.set_sampling_rate(4)
 simulator.set_migration_probability(0.01)
