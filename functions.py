c2598d638d789eb1ee691bb947e8486b1aac9516
Spirin Vadim <61345894+spirinvadim@users.noreply.github.com>
2022-01-24 19:56:47 +0300

    Updated function description

--- functions.py
+++ functions.py
@@ -56,7 +56,7 @@ def set_recovery_rate(rate, haplotype=None):
 
 	Recovery rate: the inverse of the expected recovery time. `Wikipedia`_ - parameter gamma in SIR model.
 
-	:param rate: recovery rate value.
+    :param rate: recovery rate value.
     :param haplotype: haplotypes for which the new value is being set. See XXX for details.
     :type rate: float
     :type haplotype: int or string or None
@@ -66,7 +66,7 @@ def set_sampling_rate(rate, haplotype=None):
 	"""
 	Sampling rate: the rate at which infected individuals are sampled. The genealogy will be generated only for these samples. Alternatively, one can set probability=True in order to specify the fraction of recovered individuals which are sampled.
 
-	:param rate: sampling rate value.
+    :param rate: sampling rate value.
     :param haplotype: haplotypes for which the new value is being set. See XXX for details.
     :type rate: float
     :type haplotype: int or string or None
@@ -76,11 +76,11 @@ def set_mutation_rate(rate=None, substitution_weights=None, haplotype=None, site
 	"""
 	This method allows setting the mutation rate and the weights for each single nucleotide substitution (given the mutation happened). haplotype is the ancestral haplotype for the mutation. site_id is the position on the haplotype where mutation arises. substitution_weights are given in the order of derived variant ATCG. The derived variant cannot be the same as the ancestral state, so the corresponding array entry will be ignored.
 
-	:param rate: mutation rate value (None would not change the old value).
-	:param substitution_weights: weights of each single nucleotide substitution given mutation occured (None would not change the old values). See XXX for example.
+    :param rate: mutation rate value (None would not change the old value).
+    :param substitution_weights: weights of each single nucleotide substitution given mutation occured (None would not change the old values). See XXX for example.
     :param haplotype: haplotypes for which the new value is being set. See XXX for details.
     :type rate: float or None
-	:type substitution_weights: list of four non-negative integers or None
+    :type substitution_weights: list of four non-negative integers or None
     :type haplotype: int or string or None
 	"""
 
@@ -88,7 +88,7 @@ def set_susceptibility_type(susceptibility_type, haplotype=None):
 	"""
 	The type of immunity (or the susceptibility group) which an individual gets after being infected with a pathogen of a haplotype.
 
-	:param susceptibility_type: immunity group id.
+    :param susceptibility_type: immunity group id.
     :param haplotype: haplotypes for which the new value is being set. See XXX for details.
     :type susceptibility_type: int
     :type haplotype: int or string or None
@@ -98,30 +98,24 @@ def set_susceptibility(susceptibility, haplotype=None, susceptibility_type=None)
 	"""
 	Susceptibility is a multiplicative modifier of the transmission rate based on the susceptible host immunity with a susceptibility_type. It can decrease or increase the transmission rate of a particular haplotype to the individuals of susceptibility_type.
 
-	:param susceptibility: susceptibility value.
+    :param susceptibility: susceptibility value.
     :param haplotype: haplotypes for which the new susceptibility value is being set. See XXX for details.
-	:param susceptibility_type: immunity group id for which the new susceptibility value is being set.
+    :param susceptibility_type: immunity group id for which the new susceptibility value is being set.
     :type susceptibility: int
     :type haplotype: int or string or None
-	:type susceptibility_type: int
-
-	:param: susceptibility, haplotype=None, susceptibility_type=None
-	:type: susceptibility = float, haplotype = int or str or None, susceptibility_type = int or None
+    :type susceptibility_type: int
 	"""
 
 def set_immunity_transition(rate, source=None, target=None):
 	"""
 	The change of immunity without infection (e.g. due to vaccination or immunity loss with time).
 
-	:param rate: transition rate value.
+    :param rate: transition rate value.
     :param source: source immunity group id (None means that the new value will be set to all immunity groups as source).
-	:param target: target immunity group id (None means that the new value will be set to all immunity groups as target).
+    :param target: target immunity group id (None means that the new value will be set to all immunity groups as target).
     :type rate: float
     :type source: int or None
-	:type target: int or None
-
-	:param: rate, source=None, target=None
-	:type: rate = float, source = int or None, target = int or None
+    :type target: int or None
 	"""
 
 
@@ -129,7 +123,7 @@ def set_population_size(amount, population=None):
 	"""
 	The number of individuals in the population.
 
-	:param amount: total number of individuals in the population.
+    :param amount: total number of individuals in the population.
     :param population: population for which the new population size is set (in case of None the value will be updated for all populations).
     :type amount: int
     :type population: int or None
@@ -139,7 +133,7 @@ def set_contact_density(value, population=None):
 	"""
 	The relative number of contacts per time units.
 
-	:param value: contact density value.
+    :param value: contact density value.
     :param population: population for which the new contact density size is being set (in case of None the value will be updated for all populations).
     :type value: float
     :type population: int or None
@@ -149,13 +143,13 @@ def set_susceptible_individuals(amount, source_type, target_type, population=Non
 	"""
 	The method allows to move susceptible individuals between susceptibility groups.
 
-	:param amount: the number of individuals to be moved.
+    :param amount: the number of individuals to be moved.
     :param source_type: source susceptibility group from which individuals are moved.
-	:param target_type: target susceptibility group into which individuals are moved.
+    :param target_type: target susceptibility group into which individuals are moved.
     :param population: population for which the new value is being set (in case of None the value will be updated for all populations).
     :type amount: int
     :type source_type: int or None
-	:type target_type: float
+    :type target_type: float
     :type population: int or None
 	"""
 
@@ -163,7 +157,7 @@ def set_lockdown(parameters, population=None):
 	"""
 	Setting conditions when lockdown in a population is imposed and lifted with the contact density during the lockdown.
 
-	:param parameters: list with three elements: contact density value during lockdown, fraction of infectious population when the lockdown is set, fraction of infectious population when lockdown is lifted.
+    :param parameters: list with three elements: contact density value during lockdown, fraction of infectious population when the lockdown is set, fraction of infectious population when lockdown is lifted.
     :param population: population for which the new lockdown parameters are being set (in case of None the value will be updated for all populations).
     :type parameters: list of length 3
     :type population: int or None
@@ -173,7 +167,7 @@ def set_sampling_multiplier(value, population=None):
 	"""
 	The relative sampling in the population (multiplicative modifier). Sampling rate of each haplotype is modified by this factor.
 
-	:param value: sampling multiplier value.
+    :param value: sampling multiplier value.
     :param population: population for which the new sampling multiplier is being set (in case of None the value will be updated for all populations).
     :type value: float
     :type population: int or None
@@ -183,13 +177,13 @@ def set_migration_probability(probability=None, cumulative=False, source=None, t
 	"""
 	The probability that an individual from the population source is travelling to the population target. cumulative=True means that probability is the total probability to find an individual from population source outside of its population. All the entries (except for the diagonal) of the corresponding row of the migration probability matrix will be set to probability/(K-1), where K is the number of populations in the simulation.
 
-	:param probability: probability value.
+    :param probability: probability value.
     :param cumulative: if True, all the entries (except for the diagonal element) of row corresponding to source population is filled with probabilitie/(K-1).
-	:param source: source population with infectious individual  (None means that the new value will be set to all populations as source).
+    :param source: source population with infectious individual  (None means that the new value will be set to all populations as source).
     :param target: target population with susceptible individual (None means that the new value will be set to all populations as target).
     :type probability: float
     :type cumulative: bool
-	:type source: int or None
+    :type source: int or None
     :type target: int or None
 	"""
 
@@ -199,13 +193,13 @@ def simulate(iterations=1000, sample_size=None, time=None, method='direct'):
 	This methods starts the simulation. The simulation interrupts when either one of the conditions is satisfied: the number of iterations is iterations, the number of collected samples is sample_size, ot the total virtual time of the epidemic exceeds time
 	It can be called multiple times, changes of most parameters are allowed between simulation runs.
 
-	:param iterations: maximal number of iterations.
+    :param iterations: maximal number of iterations.
     :param sample_size: desired sample size.
-	:param time: virtual (model) time to be simulated.
+    :param time: virtual (model) time to be simulated.
     :param method: 'direct' for the exact algorithm, 'tau' for tau-leaping approximated algorithm.
     :type iterations: int
     :type sample_size: int or None
-	:type time: float or None
+    :type time: float or None
     :type method: string
 	"""
 
@@ -213,7 +207,7 @@ def genealogy(seed=None):
 	"""
 	Generating a genealogy based on the chain of events generated during all the instances of simulate() method.
 
-	:param seed: seed value (None for random seed).
+    :param seed: seed value (None for random seed).
     :type seed: int or None
 	"""
 
@@ -221,24 +215,24 @@ def output_newick(name_file="newick_output"):
 	"""
 	Record format of binary trees
 
-	:param: name_file="newick_output"
-	:type: name_file = str
+    :param: name_file="newick_output"
+    :type: name_file = str
 	"""
 
 def output_mutations(name_file="mutation_output"):
 	"""
 	Information about all mutations
 
-	:param: name_file="mutation_output"
-	:type: name_file = str
+    :param: name_file="mutation_output"
+    :type: name_file = str
 	"""
 
 def output_migrations(name_file="migrations"):
 	"""
 	Information about all migrations
 
-	:param: name_file="migrations"
-	:type: name_file = str
+    :param: name_file="migrations"
+    :type: name_file = str
 	"""
 
 def output_parameters(name_file="parameters"):
