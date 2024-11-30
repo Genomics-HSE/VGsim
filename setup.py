9b6bd60ed9d80118d486a3e46daeb940e2a500dc
Vadim Spirin <vp.ypm@yandex.ru>
2024-11-30 12:57:24 +0300

    New installation library

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ import sysconfig
 import sys
 
 # Найти путь к Boost
-boost_include = '/usr/local/include'
-boost_library = '/usr/local/lib'
+# boost_include = '/usr/local/include'
+# boost_library = '/usr/local/lib'
 python_version = sys.version_info
-# boost_version = f'boost_python{python_version.major}{python_version.minor}'
-boost_version = f'boost_python{python_version.major}'
+boost_version = f'boost_python{python_version.major}{python_version.minor}'
+# boost_version = f'boost_python{python_version.major}'
 
 
 example_module = Extension('VGsim',
                             sources=['src/wrap.cpp'],
-                            include_dirs=[boost_include],
-                            library_dirs=[boost_library],
+                            # include_dirs=[boost_include],
+                            # library_dirs=[boost_library],
                             libraries=[boost_version],  # Убедитесь, что используете правильное имя библиотеки
                             extra_compile_args=['-std=c++17', '-O2'],
                             )
